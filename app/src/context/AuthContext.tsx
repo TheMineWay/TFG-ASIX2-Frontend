19c738560d826928ebe27d0b41907f90488b9525
Joel Campos <joelcamposoliva9000@gmail.com>
2022-02-28 21:54:57 +0100

    Context data update

--- app/src/context/AuthContext.tsx
+++ app/src/context/AuthContext.tsx
@@ -1,23 +1,34 @@
 import React, { useContext } from 'react';
+import { UserModel } from '../services/auth/User.model';
 
 type Props = {
     children: JSX.Element;
 }
 
 export type AuthCredentials = {
-    login: string;
     session: string;
     expiresAt: Date;
 }
 
-export const AuthCredentialsContext = React.createContext<AuthCredentials | null>(null);
+export type AuthContext = {
+    credentials: AuthCredentials;
+    user: UserModel;
+}
+
+export const AuthCredentialsContext = React.createContext<AuthContext | null>(null);
 
 export default function AuthContext(props: Props) {
     return (
         <AuthCredentialsContext.Provider value={{
-            login: "joelc",
-            session: "sessid",
-            expiresAt: new Date(),
+            credentials: {
+                session: "sessid",
+                expiresAt: new Date(),
+            },
+            user: {
+                name: "Joel",
+                lastname: "Campos",
+                login: "jcampos",
+            },
         }}>
             {props.children}
         </AuthCredentialsContext.Provider>

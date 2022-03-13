4a1f18e4b9a8e2a1c084668638e75723d124bdc9
Joel Campos <joelcamposoliva9000@gmail.com>
2022-03-13 11:58:27 +0100

    Password strength indicator

--- app/src/view/shared/PasswordStrengthIndicator.tsx
+++ app/src/view/shared/PasswordStrengthIndicator.tsx
@@ -1,18 +1,22 @@
-import { Progress } from 'antd';
-import passwordStrengthCheck from '../../services/password/passwordStrengthCheck';
+import { t } from 'i18next';
+import PasswordStrengthBar from 'react-password-strength-bar';
 
 type Props = {
     password: string;
 }
 
 export default function PasswordStrengthIndicator(props: Props) {
-    const evaluation = passwordStrengthCheck(props.password);
-
     return (
-        <Progress
-            strokeColor={evaluation.state}
-            percent={evaluation.strength}
-            showInfo={false}
+        <PasswordStrengthBar
+            password={props.password}
+            shortScoreWord={t('common.form.password.TooShort')}
+            scoreWords={[
+                t('common.form.password.TooWeak'),
+                t('common.form.password.Weak'),
+                t('common.form.password.Okay'),
+                t('common.form.password.Good'),
+                t('common.form.password.Strong')
+            ]}
         />
     );
 }

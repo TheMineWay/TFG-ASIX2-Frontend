6aae9e49a9892353d055605eeb28443212cf21fa
Joel Campos <joelcamposoliva9000@gmail.com>
2022-03-13 12:24:43 +0100

    Some validations

--- app/src/view/form/EmailFormItem.tsx
+++ app/src/view/form/EmailFormItem.tsx
@@ -1,6 +1,8 @@
 import { MailOutlined } from '@ant-design/icons';
 import { Form, Input } from 'antd';
+import { isEmail } from 'class-validator';
 import { t } from 'i18next';
+import { useState } from 'react';
 
 type Props = {
     name: string;
@@ -9,9 +11,24 @@ type Props = {
 }
 
 export default function EmailFormItem(props: Props) {
+    const [ value, setValue ] = useState<string>('');
+
+    const isValid: boolean = isEmail(value);
+
     return (
-        <Form.Item name={props.name} label={t('common.form.Email')} required={props.required && !props.requiredInvisibility}>
-            <Input prefix={<MailOutlined/>} type='email' required={props.required}/>
+        <Form.Item
+            name={props.name}
+            label={t('common.form.Email')}
+            required={props.required && !props.requiredInvisibility}
+            validateStatus={value ? (isValid ? 'success' : 'error') : 'validating'}
+        >
+            <Input
+                prefix={<MailOutlined/>}
+                type='email'
+                required={props.required}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
         </Form.Item>
     );
 }
--- app/src/view/form/PhoneFormItem.tsx
+++ app/src/view/form/PhoneFormItem.tsx
@@ -1,6 +1,8 @@
 import { PhoneOutlined } from '@ant-design/icons';
 import { Form, Input } from 'antd';
+import { isPhoneNumber } from 'class-validator';
 import { t } from 'i18next';
+import { useState } from 'react';
 
 type Props = {
     name: string;
@@ -9,9 +11,26 @@ type Props = {
 }
 
 export default function PhoneFormItem(props: Props) {
+
+    const [ value, setValue ] = useState<string>('');
+
+    const isValid: boolean = isPhoneNumber(value);
+
     return (
-        <Form.Item name={props.name} label={t('common.form.Phone')} required={props.required && !props.requiredInvisibility}>
-            <Input prefix={<PhoneOutlined/>} type='text' required={props.required} />
+        <Form.Item
+            name={props.name}
+            label={t('common.form.Phone')}
+            required={props.required && !props.requiredInvisibility}
+            validateStatus={value ? (isValid ? 'success' : 'error') : 'validating'}
+        >
+            <Input
+                value={value}
+                onChange={((e) => setValue(e.target.value))}
+                prefix={<PhoneOutlined/>}
+                type='text'
+                required={props.required}
+                placeholder='+34 555 55 55'
+            />
         </Form.Item>
     );
 }

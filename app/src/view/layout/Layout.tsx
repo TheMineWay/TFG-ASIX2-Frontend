df1aa95a5d9c64dce085865b9518c15ffa4698ca
Joel Campos <j.campos19543@culturalbadalona.com>
2022-03-30 17:35:18 +0200

    Analytics

--- app/src/view/layout/Layout.tsx
+++ app/src/view/layout/Layout.tsx
@@ -1,13 +1,24 @@
 import { Layout } from "antd";
 import { Content } from "antd/lib/layout/layout";
+import { useEffect } from "react";
 import BaseHeader from "./Header";
 import './layout.css';
+import ReactGA from 'react-ga';
+import { useLocation } from 'react-router-dom'
 
 type Props = {
     children: JSX.Element;
 }
 
 export default function BaseLayout(props: Props) {
+
+    const location = useLocation();
+    
+    useEffect(() => {
+        ReactGA.pageview(location.pathname);
+        console.log(location.pathname);
+    }, [location.pathname]);
+
     return (
         <Layout>
             <BaseHeader/>

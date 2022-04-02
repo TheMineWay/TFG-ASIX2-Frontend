e28d48b96a6a2fe2d3dfd6369aae6efcc226af56
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-02 22:58:48 +0200

    Text filter

--- app/src/view/adminInventory/AdminInventoryViewList.tsx
+++ app/src/view/adminInventory/AdminInventoryViewList.tsx
@@ -1,10 +1,11 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, Card, Col, Row, Table } from 'antd';
+import { Button, Card, Col, Input, Row, Space, Table } from 'antd';
 import { t } from 'i18next';
 import { useState } from 'react';
 import useCoins from '../../hooks/coins/useCoins';
 import useAdminInventory from '../../hooks/inventory/useAdminInventory';
 import { InventoryItem } from '../../hooks/inventory/useInventory';
+import { listFilter } from '../../services/filters/genericFilter';
 import Popconfirm from '../shared/Popconfirm';
 import AdminInventoryAddItemDrawer from './inventoryAdd/AdminInventoryAddItemDrawer';
 import AdminInventoryEditDrawer from './inventoryEditor/AdminInventoryEditDrawer';
@@ -19,6 +20,8 @@ export default function AdminInventoryViewList() {
 
     const [editingItem, setEditingItem] = useState<InventoryItem>();
     const [addItem, setAddItem] = useState<boolean>(false);
+
+    const [searchFilter, setSearchFilter] = useState<string>('');
 
     const Actions = (props: { item: InventoryItem }): JSX.Element => {
         return (
@@ -61,22 +64,29 @@ export default function AdminInventoryViewList() {
                 hide={() => setAddItem(false)}
             />
 
-            <Row gutter={[24,24]}>
+            <Row gutter={[24, 24]}>
                 <Col span={24}>
                     <Card>
-                        <Button
-                            icon={<PlusOutlined/>}
-                            type='primary'
-                            onClick={() => setAddItem(true)}
-                        >
-                            {t('view.inventory.list.actions.Add')}
-                        </Button>
+                        <Space>
+                            <Button
+                                icon={<PlusOutlined />}
+                                type='primary'
+                                onClick={() => setAddItem(true)}
+                            >
+                                {t('view.inventory.list.actions.Add')}
+                            </Button>
+                            <Input.Search
+                                allowClear
+                                onSearch={(v) => setSearchFilter(v)}
+                                defaultValue={searchFilter}
+                            />
+                        </Space>
                     </Card>
                 </Col>
                 <Col span={24}>
                     <Table
                         loading={inventory.loading}
-                        dataSource={inventory.inventory}
+                        dataSource={inventory.inventory?.filter((item) => listFilter([item.name, item.description], searchFilter))}
                     >
                         <Column
                             title={t('view.inventory.list.headers.Id')}

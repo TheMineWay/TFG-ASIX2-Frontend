ec8cac68a92d45f4436aae3faf8c8024e771ec91
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-09 12:06:41 +0200

    Retrieve sessions

--- app/src/view/sessionHistory/ViewSessionHistory.tsx
+++ app/src/view/sessionHistory/ViewSessionHistory.tsx
@@ -1,4 +1,9 @@
+import useSessionHistory from "../../hooks/sessions/useSessionHistory";
+
 export default function ViewSessionHistory() {
+
+    const sessions = useSessionHistory();
+
     return (
         <>
 

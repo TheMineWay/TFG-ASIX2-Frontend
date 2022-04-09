00c13aa0e6f341ef34fa5b745380534303c20e75
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-09 11:45:02 +0200

    Card navigator

--- app/src/view/profile/SessionsHistoryProfileCard.tsx
+++ app/src/view/profile/SessionsHistoryProfileCard.tsx
@@ -1,15 +1,24 @@
 import { Button } from 'antd';
 import { t } from 'i18next';
+import { useNavigate } from 'react-router-dom';
 import ImageSidedCard from '../shared/ImageSidedCard';
 
 export default function SessionsHistoryProfileCard() {
+
+    const navigate = useNavigate();
+
     return (
         <ImageSidedCard
             src={require('../../resources/profile/timeline/Timeline.png')}
             hoverable
             preview={false}
             actions={[
-                <Button type='primary'>Anar</Button>
+                <Button
+                    type='primary'
+                    onClick={() => navigate('/user/session-history')}
+                >
+                    {t('view.profile.sessionHistory.card.actions.Go')}
+                </Button>
             ]}
         >
             <p

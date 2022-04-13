331d58465e99e76eb9b6509d3d22fa41b55b9d7b
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-13 18:46:35 +0200

    Base builder tool

--- app/src/view/diskRequest/DiskBuilder/DiskBuilderTool.tsx
+++ app/src/view/diskRequest/DiskBuilder/DiskBuilderTool.tsx
@@ -1,7 +1,12 @@
-import { Col, Row } from "antd";
+import { Form } from "antd";
+import { useForm } from "antd/lib/form/Form";
 import useCache from "../../../hooks/cache/useCache";
 import useInventory from "../../../hooks/inventory/useInventory";
 import Loading from "../../shared/Loading";
+
+export type DiskBuilderFormValues = {
+    a: string;
+}
 
 type Props = {
     onFinish: () => void;
@@ -10,17 +15,29 @@ type Props = {
 export default function DiskBuilderTool(props: Props) {
 
     const inventory = useInventory();
-    const cache = useCache<string>({
-        cacheId: 'disk-builder'
+    const loading = inventory.loading;
+
+    const cache = useCache<DiskBuilderFormValues>({
+        cacheId: 'disk-builder',
     });
 
-    const loading = inventory.loading;
+    const [form] = useForm<DiskBuilderFormValues>();
 
     if (loading) return <Loading />;
 
+    // TODO: submit data
+    const submit = async (values: DiskBuilderFormValues): Promise<void> => {
+
+    }
+
     return (
         <>
-            
+            <Form
+                form={form}
+                onFinish={submit}
+            >
+                
+            </Form>
         </>
     );
 }

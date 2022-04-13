608b19795645e82da703805cfaa547b1b5356495
Joel Campos <joelcamposoliva9000@gmail.com>
2022-04-13 18:20:05 +0200

    Steps refactor

--- app/src/view/diskRequest/DiskBuilderTool.tsx
+++ app/src/view/diskRequest/DiskBuilderTool.tsx
@@ -1,27 +1,18 @@
 import { Col, Row } from "antd";
-import { useState } from "react";
 import useInventory from "../../hooks/inventory/useInventory";
 import Loading from "../shared/Loading";
-import DiskRequestSteps from "./DiskRequestSteps";
 
-export default function DiskBuilderTool() {
+type Props = {
+    onFinish: () => void;
+}
+
+export default function DiskBuilderTool(props: Props) {
 
     const inventory = useInventory();
-    const [step, setStep] = useState<number>(0);
 
     const loading = inventory.loading;
 
     if (loading) return <Loading />;
-
-    const next = () => {
-        if(step >= 3) return;
-        setStep(step + 1);
-    }
-
-    const back = () => {
-        if(step <= 0) return;
-        setStep(step - 1);
-    }
 
     return (
         <Row
@@ -29,9 +20,7 @@ export default function DiskBuilderTool() {
             gutter={[12,12]}
         >
             <Col span={20}>
-                <DiskRequestSteps
-                    step={step}
-                />
+
             </Col>
         </Row>
     );
--- app/src/view/diskRequest/DiskRequestViewPage.tsx
+++ app/src/view/diskRequest/DiskRequestViewPage.tsx
@@ -1,9 +1,55 @@
+import { Col, Row } from "antd";
+import { useState } from "react";
+import Container from "../shared/Container";
 import DiskBuilderTool from "./DiskBuilderTool";
+import DiskRequestSteps from "./DiskRequestSteps";
 
-export default function DiskBuilderViewPage() {
+export default function DiskRequestViewPage() {
+
+    const [step, setStep] = useState<number>(0);
+
+    const next = () => {
+        if (step >= 3) return;
+        setStep(step + 1);
+    }
+
+    const back = () => {
+        if (step <= 0) return;
+        setStep(step - 1);
+    }
+
+    const steps: {
+        step: number;
+        component: JSX.Element;
+    }[] = [
+            {
+                step: 0,
+                component: (
+                    <DiskBuilderTool
+                        onFinish={() => {
+                            next();
+                        }}
+                    />
+                )
+            }
+        ];
+
     return (
-        <>
-            <DiskBuilderTool/>
-        </>
+        <Container>
+            <Row>
+                <Col
+                    span={24}
+                >
+                    <DiskRequestSteps
+                        step={step}
+                    />
+                </Col>
+                <Col span={24}>
+                    {
+                        steps.find((s) => s.step === step)?.component
+                    }
+                </Col>
+            </Row>
+        </Container>
     );
 }

5411a4910d8a09e136b5a14d532e11108d678586
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-01 12:08:36 +0200

    Disk hook

---
+++ app/src/hooks/diskRequest/useDiskRequestList.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import request from "../../services/api/Request";
+import notificationErrorDisplay from "../../view/errors/display/NotificationErrorDisplay";
+import useAuthState from "../auth/useAuthState";
+
+export type DiskRequestState = 'pending';
+
+type DiskRequestListItem = {
+    id: string;
+    user: string;
+    createdAt: Date;
+    address: string;
+    state: DiskRequestState;
+    amount: number;
+}
+
+type RawDiskRequestListItem = {
+    id: string;
+    user: string;
+    createdAt: string;
+    address: string;
+    state: DiskRequestState;
+    amount: string;
+}
+
+export function processRawDiskRequestListItem(raw: RawDiskRequestListItem): DiskRequestListItem {
+    return {
+        ...raw,
+        createdAt: new Date(Date.parse(raw.createdAt)),
+        amount: parseFloat(raw.amount),
+    };
+}
+
+export default function useDiskRequestList() {
+    const [authState] = useAuthState();
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [list, setList] = useState<DiskRequestListItem[]>([]);
+
+    useEffect(() => {
+        fetch();
+    }, []);
+
+    async function fetch() {
+        setLoading(true);
+        try {
+            const result = await request<{ requests: RawDiskRequestListItem[] }>('post', '/actions/diskRequests/requestsList', {}, { authCredentials: authState });
+            setList(result.requests.map(processRawDiskRequestListItem));
+        } catch (e: any) {
+            notificationErrorDisplay(e);
+        }
+        setLoading(false);
+    }
+
+    return {
+        list,
+        loading,
+    }
+}

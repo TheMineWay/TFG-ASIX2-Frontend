4149d9e81545cbdce67e2b8e402212183a2e760a
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-01 14:20:32 +0200

    Display seconds

--- app/src/view/shared/DateDisplay.tsx
+++ app/src/view/shared/DateDisplay.tsx
@@ -2,19 +2,24 @@ import { Tooltip } from "antd";
 import moment from "moment";
 
 type Props = {
-    children: Date;
+    children?: Date;
     hideTime?: boolean;
+    includeSeconds?: boolean;
 }
 
 export default function DateDisplay(props: Props) {
 
+    if(!props.children) return <></>;
+
     const date = moment(props.children);
 
+    const seconds = props.includeSeconds ? ':ss' : '';
+
     const shortDate = date.format('DD/MM/YYYY');
-    const shortTime = props.hideTime ? '' : date.format(' HH:mm');
+    const shortTime = props.hideTime ? '' : date.format(` HH:mm${seconds}`);
 
     const longDate = date.format('dddd Do MMMM YYYY');
-    const longTime = props.hideTime ? '' : date.format(', h:mm a')
+    const longTime = props.hideTime ? '' : date.format(`, h:mm${seconds} a`)
     
     return (
         <Tooltip

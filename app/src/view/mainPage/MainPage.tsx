03fbc1d9177b0971a398366bb73299e02564073b
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-07 20:37:34 +0200

    Nicely set opinions

--- app/src/view/mainPage/MainPage.tsx
+++ app/src/view/mainPage/MainPage.tsx
@@ -1,11 +1,29 @@
 
+import { Col, Row } from 'antd';
 import AnnouncesCarousel from './anouncesCarousel/AnnouncesCarousel';
 import ViewPublicOpinions from './opinions/ViewPublicOpinions';
 
+const sections: JSX.Element[] = [
+    <AnnouncesCarousel />,
+    <ViewPublicOpinions />,
+];
+
 export default function MainPage() {
     return (
-        <>
-            <AnnouncesCarousel/>
-        </>
+        <Row
+            gutter={[24, 48]}
+        >
+            {
+                sections.map((section) => (
+                    <Col
+                        span={24}
+                    >
+                        {
+                            section
+                        }
+                    </Col>
+                ))
+            }
+        </Row>
     );
 }

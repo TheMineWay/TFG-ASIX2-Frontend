6d3a044e9df19a172989db7f0b94e8cbd3685ec3
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-07 19:27:18 +0200

    Random pick

--- app/src/view/mainPage/opinions/ViewPublicOpinions.tsx
+++ app/src/view/mainPage/opinions/ViewPublicOpinions.tsx
@@ -6,10 +6,25 @@ export default function ViewPublicOpinions() {
 
     const { opinions } = usePublicOpinions();
 
+    function randomSort<T = any>(array: T[]) {
+        return array.sort((a, b) => Math.random() > Math.random() ? 1 : -1);
+    }
+
+    const opinionsToVisualize = [
+        ...randomSort((opinions?.filter((o) => o.score == 5) ?? [])).slice(0, 4),
+        ...randomSort((opinions?.filter((o) => o.score == 4) ?? [])).slice(0, 1),
+        ...randomSort((opinions?.filter((o) => o.score == 3) ?? [])).slice(0, 1),
+        ...randomSort((opinions?.filter((o) => o.score == 2) ?? [])).slice(0, 1),
+        ...randomSort((opinions?.filter((o) => o.score == 1) ?? [])).slice(0, 1),
+    ];
+
     return (
-        <Row>
+        <Row
+            justify="center"
+            gutter={[24, 24]}
+        >
             {
-                opinions?.map((o) => (
+                opinionsToVisualize.map((o) => (
                     <Col
                         xs={24}
                         sm={12}

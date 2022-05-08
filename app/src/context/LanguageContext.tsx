304ea34004e7bc0da5ed00e69c6af44f7d4a3441
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-08 12:30:45 +0200

    Better language switch

--- app/src/context/LanguageContext.tsx
+++ app/src/context/LanguageContext.tsx
@@ -1,4 +1,6 @@
+import moment from 'moment';
 import React, { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 import initI18n, { Languages } from '../i18n/configureI18n';
 
 type Props = {
@@ -9,10 +11,21 @@ export const LanguageDataContext = React.createContext<[Languages, (context: Lan
 
 export default function LanguageContext(props: Props) {
 
-    const [ language, setLanguage ] = useState<Languages>(Languages.ca);
+    const [cookies, setCookie, removeCookie] = useCookies();
+    
+    const [language, setLanguage] = useState<Languages>(cookies['language'] as Languages ?? Languages.ca);
 
     useEffect(() => {
-        updateLang(Languages.ca);
+        // Expires in one year
+        setCookie('language', language, {
+            expires: moment().add('year', 1).toDate(),
+        });
+        // TODO: make optional
+        if(language !== cookies['language'] as Languages) window.location.reload();
+    }, [language]);
+
+    useEffect(() => {
+        updateLang(cookies['language'] as Languages ?? Languages.ca);
     }, []);
 
     const updateLang = async (lang: Languages) => {

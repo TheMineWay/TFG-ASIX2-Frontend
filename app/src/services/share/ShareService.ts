f311b08c12f464c314dd6927df00b46893ab5999
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-13 22:40:26 +0200

    Added content on share

--- app/src/services/share/ShareService.ts
+++ app/src/services/share/ShareService.ts
@@ -3,7 +3,9 @@ import { t } from "i18next";
 export default function shareWebsite(): void {
     if(navigator.canShare()) {
         navigator.share({
-            title: t('share.Title')
+            title: t('share.Title'),
+            text: t('share.Text'),
+            url: 'https://plugandwork.cat'
         });
     }
 }

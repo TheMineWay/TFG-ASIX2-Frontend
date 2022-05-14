3168cdae18f087d1b29b6bf0bffac5e00f890688
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-14 23:05:08 +0200

    Drawer view info

--- app/src/view/contactForm/admin/AdminContactFormViewItemDrawer.tsx
+++ app/src/view/contactForm/admin/AdminContactFormViewItemDrawer.tsx
@@ -1,5 +1,7 @@
 import { Drawer } from 'antd';
+import { t } from 'i18next';
 import { ContactFormItem } from '../../../hooks/contactForm/useAdminContactForm';
+import DateDisplay from '../../shared/DateDisplay';
 
 type Props = {
     item?: ContactFormItem;
@@ -11,8 +13,20 @@ export default function AdminContactFormViewItemDrawer(props: Props) {
         <Drawer
             visible={props.item !== undefined}
             onClose={props.hide}
+            title={<DateDisplay>{props.item?.createdAt}</DateDisplay>}
+            footer={[
+                
+            ]}
         >
-
+            <p>
+                <b>{t('view.contactForm.admin.viewItem.Name')}</b>: {props.item?.name}
+            </p>
+            <p>
+                <b>{t('view.contactForm.admin.viewItem.Email')}</b>: {props.item?.email}
+            </p>
+            <p>
+                <b>{t('view.contactForm.admin.viewItem.Message')}</b>: {props.item?.message}
+            </p>
         </Drawer>
     );
 }

aa38b41936a1dde45bf9a9884e24e4780c7e6230
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-20 11:46:22 +0200

    No date centered

--- app/src/view/shared/NoData.tsx
+++ app/src/view/shared/NoData.tsx
@@ -3,8 +3,14 @@ import { t } from 'i18next';
 
 export default function NoData() {
     return (
-        <Empty
-            description={t('common.empty.Description')}
-        />
+        <div
+            style={{
+                width: '100%',
+            }}
+        >
+            <Empty
+                description={t('common.empty.Description')}
+            />
+        </div>
     );
 }

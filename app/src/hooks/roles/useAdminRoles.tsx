ec7b398e7f81284347056af92600a703bb2bf0b4
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-20 11:45:38 +0200

    Display roles and perms

--- app/src/hooks/roles/useAdminRoles.tsx
+++ app/src/hooks/roles/useAdminRoles.tsx
@@ -1,25 +1,54 @@
+import { useState } from "react";
+import request from "../../services/api/Request";
 import { Permissions } from "../../services/security/permissions";
+import notificationErrorDisplay from "../../view/errors/display/NotificationErrorDisplay";
 import useAuthState from "../auth/useAuthState";
-import usePermissions from "./usePermissions";
+import usePermissions, { Permission } from "./usePermissions";
 import useRoles from "./useRoles";
 
 export default function useAdminRoles() {
 
     const { permissions, convertByPermissionId } = usePermissions();
     const { roles } = useRoles();
+    const [loadingState, setLoadingState] = useState<{ [role: string]: boolean }>({});
+
+    function isLoading(id: string): boolean {
+        return Object.keys(loadingState).includes(id) ? loadingState[id] : false;
+    }
+
+    function setLoading(id: string, loading: boolean = true) {
+        const state = loadingState;
+
+        state[id] = loading;
+
+        setLoadingState({
+            ...state,
+        });
+    }
 
     const [authState] = useAuthState();
 
     function permissionsByRole(role: string): Permissions[] {
         const r = roles?.find((ro) => ro.id === role);
-        if(!r) return [];
+        if (!r) return [];
 
         return r.permissions.map(convertByPermissionId);
+    }
+
+    async function setRolePermissions(role: string, permissions: Permission[]) {
+        setLoading(role);
+        try {
+            await request<{}>('post', '/actions/admin/roles/setPermissions', { role, permissions: permissions.map((p) => p.id) }, { authCredentials: authState });
+        } catch (e: any) {
+            notificationErrorDisplay(e);
+        }
+        setLoading(role, false);
     }
 
     return {
         permissionsByRole,
         roles,
         permissions,
+        setRolePermissions,
     }
 }
--- app/src/view/adminRoles/AdminRolesViewPage.tsx
+++ app/src/view/adminRoles/AdminRolesViewPage.tsx
@@ -1,12 +1,109 @@
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
+import { Card, Col, Row, Select, Switch } from "antd";
+import { useState } from "react";
 import useAdminRoles from "../../hooks/roles/useAdminRoles";
+import Loading from "../shared/Loading";
+import NoData from "../shared/NoData";
 
 export default function AdminRolesViewPage() {
+    const {
+        permissions,
+        roles,
+    } = useAdminRoles();
 
-    const {} = useAdminRoles();
+    const [selectedRole, setSelectedRole] = useState<string | undefined>();
+
+    if (!roles || !permissions) return <Loading />
+
+    const RolesSelect = () => (
+        <Select<string>
+            style={{
+                width: '100%',
+            }}
+            onChange={(v) => setSelectedRole(v)}
+            value={selectedRole}
+        >
+            {
+                roles.map((role) => (
+                    <Select.Option
+                        key={role.id}
+                    >
+                        {role.name}
+                    </Select.Option>
+                ))
+            }
+        </Select>
+    );
+
+    const PermissionSelector = () => (
+        <Row
+            gutter={[12, 12]}
+        >
+            {
+                permissions.map((perm) => (
+                    <Col
+                        xs={12}
+                        lg={8}
+                        xl={6}
+                        xxl={4}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'stretch',
+                            width: '100%',
+                        }}
+                    >
+                        <Card
+                            style={{
+                                width: '100%',
+                            }}
+                            bodyStyle={{
+                                display: 'flex',
+                                gap: 12,
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Switch
+                                checkedChildren={<CheckOutlined />}
+                                unCheckedChildren={<CloseOutlined />}
+                            />
+                            <>{perm.name}</>
+                        </Card>
+                    </Col>
+                ))
+            }
+        </Row>
+    );
 
     return (
-        <>
-
-        </>
+        <Row
+            gutter={[24, 24]}
+        >
+            <Col span={24}>
+                <Card>
+                    <Row
+                        gutter={[12, 12]}
+                        justify='center'
+                    >
+                        <Col
+                            xs={24}
+                            sm={12}
+                            lg={6}
+                            xl={4}
+                        >
+                            <RolesSelect />
+                        </Col>
+                    </Row>
+                </Card>
+            </Col>
+            {
+                selectedRole ? (
+                    <Col span={24}>
+                        <PermissionSelector />
+                    </Col>
+                ) : (
+                    <NoData/>
+                )
+            }
+        </Row>
     );
 }

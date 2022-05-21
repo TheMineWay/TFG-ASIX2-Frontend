779cab1089f9fe96ee5a2771fe479792a12daa9a
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-21 18:33:03 +0200

    IVA detail

--- app/src/hooks/coins/useCoins.tsx
+++ app/src/hooks/coins/useCoins.tsx
@@ -1,17 +1,24 @@
 import { Avatar, List, Tooltip } from 'antd';
+import { t } from 'i18next';
 import useCoinsPrice from './useCoinsPrice';
 
 export default function useCoins() {
-    
-    const DisplayPrice = (props: {price: number}): JSX.Element => {
+
+    const DisplayPrice = (props: { price: number }): JSX.Element => {
         const { coins } = useCoinsPrice();
+
+        const iva = 21;
+        const price = Math.round(props.price * 100) / 100;
+        const priceWithIva = Math.round(((price * (iva / 100)) + price) * 100) / 100;
+
+        const showIva = true;
 
         return (
             <Tooltip
                 color='white'
                 title={(
                     <List
-                        style={{width: 300}}
+                        style={{ width: 300 }}
                         itemLayout='horizontal'
                     >
                         {
@@ -19,7 +26,7 @@ export default function useCoins() {
                                 <List.Item>
                                     <List.Item.Meta
                                         title={coin.name}
-                                        avatar={<Avatar src={coin.imageUrl}/>}
+                                        avatar={<Avatar src={coin.imageUrl} />}
                                         description={coin.price * props.price}
                                     />
                                 </List.Item>
@@ -28,7 +35,19 @@ export default function useCoins() {
                     </List>
                 )}
             >
-                {`${Math.round(props.price * 100) / 100}€`}
+                <div
+                    style={{
+                        display: 'flex',
+                        gap: 5,
+                    }}
+                >
+                    <p>{priceWithIva}€</p>
+                    {
+                        showIva && (
+                            <small>{t('common.other.WithoutIva')}: {price}€</small>
+                        )
+                    }
+                </div>
             </Tooltip>
         );
     }

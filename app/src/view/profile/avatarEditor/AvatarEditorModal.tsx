bab05f18e0195af1a993efba46371b3ddbf08685
Joel Campos <joelcamposoliva9000@gmail.com>
2022-05-22 16:57:41 +0200

    Editor

--- app/src/view/profile/avatarEditor/AvatarEditorModal.tsx
+++ app/src/view/profile/avatarEditor/AvatarEditorModal.tsx
@@ -1,4 +1,5 @@
-import { Modal } from 'antd';
+import { Alert, Modal } from 'antd';
+import { t } from 'i18next';
 import React from 'react'
 import { getBaseUrl } from '../../../conf/conf';
 import useAuthState from '../../../hooks/auth/useAuthState';
@@ -14,14 +15,26 @@ export default function AvatarEditorModal(props: Props) {
 
     const [authState] = useAuthState();
 
-    if(!authState?.session) return <Loading/>;
+    if (!authState?.session) return <Loading />;
 
     return (
         <Modal
             visible={props.visible}
             onCancel={props.hide}
+            onOk={props.hide}
             closable={false}
         >
+            <Alert
+                type='warning'
+                showIcon
+                message={(
+                    <>
+                        <b>{t('warnings.cors.Title')}</b>
+                        <p>{t('warnings.cors.Message')}</p>
+                    </>
+                )}
+            />
+            <br />
             <iframe
                 src={getBaseUrl() + `/actions/me/uploadAvatarForm.php?&session=${authState?.session}`}
             />

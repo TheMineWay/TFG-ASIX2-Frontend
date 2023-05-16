16cc332b52c7e9b2645b1c6af9bfccdbe78b4f7a
Joel Campos <joelcamposoliva9000@gmail.com>
2023-05-16 22:15:27 +0200

    Update package lock

--- app/src/view/profile/OpinionProfileCard.tsx
+++ app/src/view/profile/OpinionProfileCard.tsx
@@ -1,31 +1,30 @@
-import { Card, notification, Skeleton } from 'antd';
-import useAuthState from '../../hooks/auth/useAuthState';
-import useMyOpinion from '../../hooks/opinions/useMyOpinion';
-import OpinionForm from '../layout/opinions/OpinionForm';
+import { Card, notification } from "antd";
+import useAuthState from "../../hooks/auth/useAuthState";
+import useMyOpinion from "../../hooks/opinions/useMyOpinion";
+import OpinionForm from "../layout/opinions/OpinionForm";
 
 export default function OpinionProfileCard() {
+  const [authState] = useAuthState();
+  const { opinion, loading } = useMyOpinion();
 
-    const [authState] = useAuthState();
-    const { opinion, loading } = useMyOpinion();
-
-    return (
-        <Card
-            hoverable
-            loading={loading}
-            onMouseEnter={() => notification.close('opinion-notification')}
-        >
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}
-            >
-            <OpinionForm
-                authState={authState}
-                defaultValues={opinion ?? undefined}
-                center
-            />
-            </div>
-        </Card>
-    );
+  return (
+    <Card
+      hoverable
+      loading={loading}
+      onMouseEnter={() => notification.close("opinion-notification")}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <OpinionForm
+          authState={authState}
+          defaultValues={opinion ?? undefined}
+          center
+        />
+      </div>
+    </Card>
+  );
 }
